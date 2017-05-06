f7b521b4cf839363283bb9752be5970849154d84
Marius Ungureanu <marius.ungureanu@xamarin.com>
2017-05-06 18:30:17 +0300

    Initial commit

---
+++ SyntaxVisualizer/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "SyntaxVisualizer",
+    Namespace = "SyntaxVisualizer",
+    Version = "1.0"
+)]
+
+[assembly: AddinName("SyntaxVisualizer")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("SyntaxVisualizer")]
+[assembly: AddinAuthor("Marius Ungureanu")]
